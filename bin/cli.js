9033f14de8ae625f2fd0ce4b40d51be0b438be90
Mateus  Tavares <mateu_sousa_tavares@hotmail.com>
2025-04-10 12:11:41 -0300

    fix: update import path for generateExports and enhance error handling in cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { generateExports } from './generate';
+import { generateExports } from './index';
 
 const targetDir = process.argv[2];
 
@@ -8,4 +8,11 @@ if (!targetDir) {
     console.error('Por favor, forneça o nome da pasta como argumento.');
     process.exit(1);
 }
-generateExports(targetDir);
+
+try {
+    generateExports(targetDir);
+    console.log(`index.ts gerado com sucesso para ${targetDir}`);
+} catch (err) {
+    console.error('Erro ao gerar exports:', err);
+    process.exit(1);
+}
